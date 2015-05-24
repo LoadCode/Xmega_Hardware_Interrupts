aa839aaa2666db38e336a2a031ad59b25910ef69
Julio Marulanda <julio.marulanda@outlook.com>
2015-05-24 10:36:43 -0500

    the interrupt configuration function, now configures int0 or int1 in any pin and any mode (falling edge, etc)

--- src/Xmega_pins_Interrupt.c
+++ src/Xmega_pins_Interrupt.c
@@ -10,13 +10,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-void InterruptConfig(PORT_t *PORT)
+void InterruptConfig(PORT_t *PORT, uint8_t pin, uint8_t pin_bm ,uint8_t mode_gc, uint8_t InterruptNumber)
 {
-	//this function configures a basic interrupt 
-	//interrupt is present in PC2
-	PORT->PIN2CTRL |= PORT_ISC_FALLING_gc; //Activate interrupt on falling
+	//this function configures a interrupt (InterruptNumber->0 1) in the 'pin' of the port with direction PORT
+	// with the mode 'mode_gc' example: PORT_ISC_FALLING_gc
+	*(&(PORT->PIN0CTRL)+pin) |= mode_gc; //Activate interrupt on falling
 	PORT->INTCTRL   = PORT_INT0LVL_MED_gc; //Config the priority of interrupt
-	PORT->INT0MASK = PIN2_bm;			   //Interrupt0 input on pin 2
+	*(&(PORT->INT0MASK)+InterruptNumber) = pin_bm; //Interrupt0 or Interrupt1 input on pin 'pin'
 	PMIC.CTRL |= PMIC_MEDLVLEN_bm;		   //Set the level of priority on the
 	//Programable Multilevel Interrupt Controller
 	sei();
@@ -26,15 +26,13 @@ int main(void)
 {
 	//code tested on Xmega32A4U
 	PORTA.DIR = 0xFF; //port A as output for indication in a led on pin 1
-	InterruptConfig(&PORTC); //Cofig interrupt in pin 2 of portc on falling edge
-	
-    while(1)
-    { 
-    }
+	//Configures interrupt 0 input in pin 2 of the port B in falling edge
+	InterruptConfig(&PORTB,pin,PIN2_bm,PORT_ISC_FALLING_gc,0);
+    while(1);
 }
 
 ISR(PORTC_INT0_vect) //ISR for the interrupt0 in PORTC
 {
-	PORTA.OUTTGL = 0x01;
-	//PORTC.INTFLAGS = PORT_INT0IF_bm; //clears the interrupt 0 flag for debounce case
+	PORTA.OUTTGL = 0x0F;
+	PORTC.INTFLAGS = PORT_INT0IF_bm; //clears the interrupt 0 flag for debounce case
 }
